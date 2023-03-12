8d20b8e18c208fd7db6c8d5cd6f1f24c95155ec6
zglian <90401613+zglian@users.noreply.github.com>
2023-03-13 02:03:32 +0800

    Update full-test.sh

--- full-test.sh
+++ full-test.sh
@@ -28,16 +28,16 @@ fi
 
 # If there's a configuration for the assignment number, use this to look for
 # additional tests
-/*if [ -f conf/assignment.txt ]; then
+if [ -f conf/assignment.txt ]; then
     # This is just one example of how you could find an associated assignment
     assignment=`cat conf/assignment.txt`
     if [ -f ./assignment-autotest/test/${assignment}/assignment-test.sh ]; then
         echo "Executing assignment test script"
         ./assignment-autotest/test/${assignment}/assignment-test.sh $test_dir
         rc=$?
-        if [ $rc -eq 0 ]; then*/
+        if [ $rc -eq 0 ]; then
             echo "Test of assignment ${assignment} complete with success"
-/*        else
+        else
             echo "Test of assignment ${assignment} failed with rc=${rc}"
             exit $rc
         fi
@@ -48,5 +48,5 @@ fi
 else
     echo "Missing conf/assignment.txt, no assignment to run"
     exit 1
-fi*/
+fi
 exit ${unit_test_rc}
